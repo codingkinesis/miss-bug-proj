917a9c0b31b2dfe5ba6f0f4443bf96f0c8bdc92f
codingkinesis <ilanbarcohn@gmail.com>
2024-02-20 16:28:47 +0200

    build

--- miss-bug-proj back/public/js/bugs.js
+++
@@ -1,9 +0,0 @@
-
-async function getBugs() {
-    const elPre = document.querySelector('pre')
-
-    const res = await fetch('bugs')
-    const bugs = await res.json()
-
-    elPre.innerText = JSON.stringify(bugs, null, 4)
-}
